e8f7515fbdf01cfe64229b51a6177d33f1260c9a
John Bogovic <bogovicj@janelia.hhmi.org>
2017-08-22 15:59:01 -0400

    compartment hist now also includes mask
    * also add histogram scripts

--- src/main/java/net/imglib2/histogram/BuildCompartmentHistograms.java
+++ src/main/java/net/imglib2/histogram/BuildCompartmentHistograms.java
@@ -5,14 +5,21 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.adobe.xmp.impl.Utils;
+import com.google.common.collect.Streams;
 
 import ij.IJ;
 import ij.ImagePlus;
 import io.nii.NiftiIo;
 import loci.formats.FormatException;
+import net.imglib2.AbstractCursor;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccess;
@@ -20,18 +27,24 @@ import net.imglib2.converter.Converter;
 import net.imglib2.converter.Converters;
 import net.imglib2.filter.MaskedIterableFilter;
 import net.imglib2.filter.MaskedIterableFilter.MaskedIterator;
+import net.imglib2.img.AbstractImg;
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.ByteImagePlus;
 import net.imglib2.img.imageplus.ShortImagePlus;
+import net.imglib2.type.BooleanType;
 import net.imglib2.type.logic.BoolType;
 import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Util;
 
 
 public class BuildCompartmentHistograms
@@ -44,18 +57,24 @@ public class BuildCompartmentHistograms
 		final float histmin = Float.parseFloat( args[ 2 ]);
 		final float histmax = Float.parseFloat( args[ 3 ]);
 		final int numBins = Integer.parseInt( args[ 4 ]);
-		final String maskF = args[ 5 ];
-		
-		boolean tailBins = false;
-		if( args.length >= 8 )
-			tailBins = Boolean.parseBoolean( args[ 7 ] );
+		final String compartmentF = args[ 5 ];
+		final String maskF = args[ 6 ];
+		final boolean tailBins = Boolean.parseBoolean( args[ 7 ] );
+
+		List<Integer> labellist = null;
+		if( args.length >= 9 )
+		{
+			labellist = parseLabels( args[ 8 ]);
+			System.out.println("labellist: " + labellist );
+		}
 
 		System.out.println( "tailBins: " + tailBins );
 		Real1dBinMapper<FloatType> binMapper = new Real1dBinMapper<FloatType>(
 				histmin, histmax, numBins, tailBins );
 
-		System.out.println( "imF   : " + imF );
-		System.out.println( "maskF : " + maskF );
+		System.out.println( "imF          : " + imF );
+		System.out.println( "maskF        : " + maskF );
+		System.out.println( "compartmentF : " + compartmentF );
 		ImagePlus imp = null;
 		if( imF.endsWith( "nii" ))
 		{
@@ -66,18 +85,35 @@ public class BuildCompartmentHistograms
 			imp = IJ.openImage( imF );
 		}
 	
-		ImagePlus maskImp = null;
+		IterableInterval<BoolType> maskImg = null;
 		if( maskF.endsWith( "nii" ))
 		{
-			maskImp = ( NiftiIo.readNifti( new File( maskF )));
+			maskImg = getMask( ImageJFunctions.convertFloat( NiftiIo.readNifti( new File( maskF ))), 0.0 );
 		}
 		else
 		{
-			maskImp = ( IJ.openImage( maskF ) );	
+			maskImg = getMask( ImageJFunctions.convertFloat( IJ.openImage( maskF )), 0.0 );
 		}
 
-		findAllHistograms( imp, maskImp, binMapper, outF );
+		ImagePlus compartmentImg = null;
+		if( compartmentF.endsWith( "nii" ))
+		{
+			compartmentImg = ( NiftiIo.readNifti( new File( compartmentF )));
+		}
+		else
+		{
+			compartmentImg = ( IJ.openImage( compartmentF ) );
+		}
 
+		findAllHistograms( imp, maskImg, compartmentImg, binMapper, outF, labellist );
+
+	}
+
+	public static List<Integer> parseLabels( String label )
+	{
+		return Arrays.stream( label.split( "," ))
+				.map( Integer::parseInt )
+				.collect( Collectors.toList() );
 	}
 
 	public static <T extends IntegerType<T>> IterableInterval<BoolType> getMask( IterableInterval<T> img, T i )
@@ -100,20 +136,54 @@ public class BuildCompartmentHistograms
 				new BoolType());
 	}
 
-	public static void findAllHistograms( ImagePlus imgImp, ImagePlus maskimp, Real1dBinMapper<FloatType> mapper, String outF )
+	public static <T extends RealType<T>> IterableInterval<BoolType> getMask( IterableInterval<T> img, double thresh )
+	{
+		return Converters.convert( img, 
+				new Converter<T,BoolType>()
+				{
+					@Override
+					public void convert( T input, BoolType output) {
+						if( input.getRealDouble() > thresh )
+						{
+							output.set( true );
+						}
+						else
+						{
+							output.set( false );
+						}
+					}
+				}, 
+				new BoolType());
+	}
+
+	public static void findAllHistograms( ImagePlus imgImp, IterableInterval< BoolType > maskImg, ImagePlus compartmentImp, 
+			Real1dBinMapper<FloatType> mapper, 
+			String outF, List<Integer> labellist )
 	{
 		Img< FloatType > img = ImageJFunctions.convertFloat( imgImp );
-		if( maskimp.getType() == ImagePlus.GRAY8 )
+		if( compartmentImp.getType() == ImagePlus.GRAY8 )
 		{
-			ByteImagePlus< UnsignedByteType > labels = ImagePlusAdapter.wrapByte( maskimp );
-			Collection<UnsignedByteType> uniques = uniqueValues( labels );
-			findAllHistograms( img, labels, uniques, mapper, outF );
+			ByteImagePlus< UnsignedByteType > labels = ImagePlusAdapter.wrapByte( compartmentImp );
+			Collection<UnsignedByteType> uniques;
+			if( labellist == null)
+				uniques = uniqueValues( labels );
+			else
+				uniques = labellist.parallelStream().map( UnsignedByteType::new )
+						.collect( Collectors.toList() );
+
+			findAllHistograms( img, maskImg, labels, uniques, mapper, outF, labellist );
 		}
-		else if( maskimp.getType() == ImagePlus.GRAY16 )
+		else if( compartmentImp.getType() == ImagePlus.GRAY16 )
 		{
-			ShortImagePlus< UnsignedShortType > labels = ImagePlusAdapter.wrapShort( maskimp );
-			Collection<UnsignedShortType> uniques = uniqueValues( labels );
-			findAllHistograms( img, labels, uniques, mapper, outF );
+			ShortImagePlus< UnsignedShortType > labels = ImagePlusAdapter.wrapShort( compartmentImp );
+			Collection<UnsignedShortType> uniques;
+			if( labellist == null)
+				uniques = uniqueValues( labels );
+			else
+				uniques = labellist.parallelStream().map( UnsignedShortType::new )
+						.collect( Collectors.toList() );
+
+			findAllHistograms( img, maskImg, labels, uniques, mapper, outF, labellist );
 		}
 		else
 		{
@@ -122,17 +192,19 @@ public class BuildCompartmentHistograms
 	}
 	
 	public static <I extends IntegerType<I>, T extends RealType<T>> void findAllHistograms( 
-			Img<T> img, Img<I> labels, Collection<I> uniques, Real1dBinMapper<T> binMapper, String outF )
+			Img<T> img, IterableInterval<BoolType> mask, Img<I> labels, Collection<I> uniques, Real1dBinMapper<T> binMapper,
+			String outF, List<Integer> labellist )
 	{
+		T t = img.firstElement().copy();
 		System.out.println( uniques );
 		for( I i : uniques )
 		{
 			System.out.println( i );
-			IterableInterval<BoolType> mask = getMask( labels, i );
+			IterableInterval<BoolType> compartmentAndMask = new And( getMask( labels, i ), mask );
 
 			final Histogram1d<T> hist = new Histogram1d<>( binMapper );
 			MaskedIterableFilter<T,BoolType> mit = 
-					new MaskedIterableFilter<T,BoolType>( mask.iterator(), img );
+					new MaskedIterableFilter<T,BoolType>( compartmentAndMask.iterator(), img );
 
 			hist.countData( mit );
 			MaskedIterator<FloatType,BoolType> mi = (MaskedIterator<FloatType,BoolType>)mit.iterator();
@@ -145,7 +217,7 @@ public class BuildCompartmentHistograms
 			String outputPath = outF + "_" + i.toString() + ".csv";
 			System.out.println( "outputPath: " + outputPath );
 
-			writeHistogram( outputPath, hist );
+			writeHistogram( outputPath, hist, t );
 		}
 	}
 	
@@ -161,9 +233,8 @@ public class BuildCompartmentHistograms
 		return uniqueList;
 	}
 	
-	public static <T extends RealType<T>> boolean writeHistogram( String outPath, Histogram1d<T> hist )
+	public static <T extends RealType<T>> boolean writeHistogram( String outPath, Histogram1d<T> hist, T t )
 	{
-		T t = hist.firstDataValue().copy();
 		RandomAccess<LongType> hra = hist.randomAccess();
 		try
 		{
@@ -184,5 +255,114 @@ public class BuildCompartmentHistograms
 		}
         return true;
 	}
+
+	public static class And extends AbstractImg<BoolType>
+	{
+		IterableInterval<BoolType> a;
+		IterableInterval<BoolType> b;
+		public And( IterableInterval<BoolType> a, IterableInterval<BoolType> b )
+		{
+			super( Intervals.dimensionsAsLongArray( a ));
+			this.a = a;
+			this.b = b;
+		}
+		@Override
+		public ImgFactory< BoolType > factory()
+		{
+			return null;
+		}
+		@Override
+		public Img< BoolType > copy()
+		{
+			return this;
+		}
+		@Override
+		public RandomAccess< BoolType > randomAccess()
+		{
+			return null;
+		}
+		@Override
+		public Cursor< BoolType > cursor()
+		{
+			return new AndCursor< BoolType >( a.cursor(), b.cursor(), new BoolType() );
+		}
+		@Override
+		public Cursor< BoolType > localizingCursor()
+		{
+			return null;
+		}
+		@Override
+		public Object iterationOrder()
+		{
+			return null;
+		}
+	};
+
+	public static class AndCursor <T extends BooleanType<T>> extends AbstractCursor<T>
+	{
+		final Cursor<T> a;
+		final Cursor<T> b;
+		T t;
+		public AndCursor( Cursor<T> a, Cursor<T> b, T t  )
+		{
+			super( a.numDimensions() );
+			this.a = a;
+			this.b = b;
+			this.t = t;
+		}
+
+		@Override
+		public T get()
+		{
+			t.set( a.get() );
+			t.and( b.get() );
+			return t;
+		}
+
+		@Override
+		public void fwd()
+		{
+			a.fwd();
+			b.fwd();
+		}
+
+		@Override
+		public void reset()
+		{
+			a.reset();
+			b.reset();
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return a.hasNext() && b.hasNext();
+		}
+
+		@Override
+		public void localize( long[] position )
+		{
+			a.localize( position );
+		}
+
+		@Override
+		public long getLongPosition( int d )
+		{
+			return a.getLongPosition( d );
+		}
+
+		@Override
+		public AbstractCursor<T> copy()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public AbstractCursor<T> copyCursor()
+		{
+			return this;
+		}
+	};
 
 }
