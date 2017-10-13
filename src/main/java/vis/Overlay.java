3265c4dc350d593639caf8b2abf126f330fff40a
John Bogovic <bogovicj@janelia.hhmi.org>
2017-10-13 10:30:46 -0400

    make overlay work with nii's

--- src/main/java/vis/Overlay.java
+++ src/main/java/vis/Overlay.java
@@ -1,24 +1,39 @@
 package vis;
 
+import java.io.File;
+import java.io.IOException;
+
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.RGBStackMerge;
+import io.nii.NiftiIo;
+import loci.formats.FormatException;
 
 public class Overlay
 {
 
 	static final String PROP_FORMAT ="channels=1 slices=%d frames=1 unit=pixel pixel_width=1.0000 pixel_height=1.0000 voxel_depth=1.0000";
 	static final String MERGE_FORMAT ="c2=%s c6=%s create keep ignore";
-	public static void main( String[] args )
+	public static void main( String[] args ) throws FormatException, IOException
 	{
 		String outpath = args[ 0 ];
 
-		ImagePlus a = IJ.openImage( args[ 1 ]);
+		ImagePlus a = null;
+		if( args[ 1 ].endsWith( "nii" ))
+			a = NiftiIo.readNifti( new File( args[1] ) );
+		else
+			a = IJ.openImage( args[ 1 ]);
+
 		IJ.run( a, "Properties...", String.format( PROP_FORMAT, a.getStackSize() ));
 		a.setSlice( a.getStackSize() / 2 );
 		IJ.run( a, "Enhance Contrast", "saturated=0.35");
 
-		ImagePlus b = IJ.openImage( args[ 2 ]);
+		ImagePlus b = null;
+		if( args[ 2 ].endsWith( "nii" ))
+			b = NiftiIo.readNifti( new File( args[ 2 ] ) );
+		else
+			b = IJ.openImage( args[ 2 ]);
+
 		IJ.run( b, "Properties...", String.format( PROP_FORMAT, b.getStackSize() ));
 		b.setSlice( a.getStackSize() / 2 );
 		IJ.run( b, "Enhance Contrast", "saturated=0.35");
