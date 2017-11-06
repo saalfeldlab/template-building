70001d549e94863540de8a002f553d67a00a55ae
John Bogovic <bogovicj@janelia.hhmi.org>
2017-11-06 16:51:52 -0500

    render works better with calibrated images

--- src/main/java/process/RenderTransformed.java
+++ src/main/java/process/RenderTransformed.java
@@ -2,12 +2,9 @@ package process;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.janelia.utility.parse.ParseUtils;
 
@@ -18,20 +15,12 @@ import ij.ImagePlus;
 import io.AffineImglib2IO;
 import io.nii.NiftiIo;
 import loci.formats.FormatException;
-import net.imglib2.Cursor;
 import net.imglib2.FinalInterval;
-import net.imglib2.Interval;
-import net.imglib2.RandomAccess;
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.converter.Converter;
-import net.imglib2.converter.Converters;
 import net.imglib2.exception.ImgLibException;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.FloatImagePlus;
 import net.imglib2.img.imageplus.ImagePlusImgs;
-import net.imglib2.img.imageplus.ShortImagePlus;
 import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.realtransform.AffineTransform;
 import net.imglib2.realtransform.AffineTransform3D;
@@ -42,35 +31,18 @@ import net.imglib2.realtransform.InvertibleRealTransformSequence;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.realtransform.ants.ANTSDeformationField;
 import net.imglib2.realtransform.ants.ANTSLoadAffine;
-import net.imglib2.transform.InverseTransform;
-import net.imglib2.type.numeric.NumericType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.ShortType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
 import net.imglib2.util.Util;
 import net.imglib2.view.IntervalView;
-import net.imglib2.view.RandomAccessibleOnRealRandomAccessible;
 import net.imglib2.view.Views;
-import transforms.AffineHelper;
 import util.RenderUtil;
 
 /**
- * Renders images transformed/registered at low resolution at high resolution.
- * <p>
- * Applies up to four transformations:
- * <ul>
- * 	<li>(Optional) "Downsampling Affine" : affine from high (original) resolution to low resoultion (at which registration was performed)</li>
- *  <li>"Reg affine" : Affine part of registering transform</li>
- *  <li>"Reg Warp" : Deformable part of registering transform</li>
- *  <li>(Optional) "to canonical affine" : Final transform in final high-res space (usually brining subject to a "canonical" orientation" </li>
- * </ul>
- * <p>
+ * Renders images transformed/registered.
+ * 
  * Currently the final (implied) upsampling transform is hard-coded in : it resmples up by a factor of 4 in x and y and 2 in z.
  * After that, it upsamples z again by a factor of (~ 2.02) to resample the images to a resolution of 0.188268 um isotropic.
  * 
- * Command line parameters / usage:
- * RenderHires <high res image> <downsample affine> <reg affine> <reg warp> <output interval> <output path> <optional to canonical affine>
  * 
  * @author John Bogovic
  *
@@ -87,14 +59,45 @@ public class RenderTransformed
 		
 		System.out.println( "imf: " + imF );
 		// LOAD THE IMAGE
-		Img< FloatType > baseImg = null;
+
+		ImagePlus ip = null;
+		AffineTransform3D resInXfm = null;
 		if( imF.endsWith( "nii" ))
 		{
-			baseImg = ImageJFunctions.convertFloat( NiftiIo.readNifti( new File( imF )));
+			ip = NiftiIo.readNifti( new File( imF ));
 		}
 		else
 		{
-			baseImg = ImageJFunctions.convertFloat( IJ.openImage( imF ) );	
+			ip = IJ.openImage( imF );
+		}
+
+		Img< FloatType > baseImg = ImageJFunctions.convertFloat( IJ.openImage( imF ) );
+
+
+		double rx = ip.getCalibration().pixelWidth;
+		double ry = ip.getCalibration().pixelHeight;
+		double rz = ip.getCalibration().pixelDepth;
+
+
+		if( rx == 0 ){
+			rx = 1.0;
+			System.err.println( "WARNING: rx = 0 setting to 1.0" );
+		}
+		if( ry == 0 ){
+			ry = 1.0;
+			System.err.println( "WARNING: ry = 0 setting to 1.0" );
+		}
+		if( rz == 0 ){
+			rz = 1.0;
+			System.err.println( "WARNING: rz = 0 setting to 1.0" );
+		}
+
+		if( rx != 1.0  || ry != 1.0 || rz != 1.0 )
+		{
+			resInXfm = new AffineTransform3D();
+			resInXfm.set( 	rx, 0.0, 0.0, 0.0, 
+					  		0.0, ry, 0.0, 0.0, 
+					  		0.0, 0.0, rz, 0.0 );
 		}
 
 		FinalInterval renderInterval = parseInterval( outputIntervalF );
@@ -104,7 +107,14 @@ public class RenderTransformed
 		// Concatenate all the transforms
 		InvertibleRealTransformSequence totalXfm = new InvertibleRealTransformSequence();
 
+		if( resInXfm != null )
+		{
+			totalXfm.add( resInXfm );
+			System.out.println( "resInXfm: " + resInXfm );
+		}
+
 		int nThreads = 8;
+		AffineTransform3D resOutXfm = null;
 
 		int i = 3;
 		while( i < args.length )
@@ -124,14 +134,29 @@ public class RenderTransformed
 				System.out.println( "argument specifies " + nThreads + " threads" );
 				continue;
 			}
-			
+
+			if( args[ i ].equals( "-r" ))
+			{
+				i++;
+				double[] outputResolution = ParseUtils.parseDoubleArray( args[ i ] );
+				i++;
+
+				resOutXfm = new AffineTransform3D();
+				resOutXfm.set( 	outputResolution[ 0 ], 0.0, 0.0, 0.0, 
+						  		0.0, outputResolution[ 1 ], 0.0, 0.0, 
+						  		0.0, 0.0, outputResolution[ 2 ], 0.0 );
+
+				System.out.println( "output Resolution " + Arrays.toString( outputResolution ));
+				continue;
+			}
+
 			if( invert )
 				System.out.println( "loading transform from " + args[ i ] + " AND INVERTING" );
 			else
 				System.out.println( "loading transform from " + args[ i ]);
 			
 			InvertibleRealTransform xfm = loadTransform( args[ i ], invert );
-			
+
 			if( xfm == null )
 			{
 				System.err.println("  failed to load transform ");
@@ -141,6 +166,10 @@ public class RenderTransformed
 			totalXfm.add( xfm );
 			i++;
 		}
+
+		if( resOutXfm != null )
+			totalXfm.add( resOutXfm.inverse() );
+
 
 		System.out.println("transforming");
 		IntervalView< FloatType > imgHiXfm = Views.interval( 
@@ -153,6 +182,7 @@ public class RenderTransformed
 //		Bdv bdv = BdvFunctions.show( imgHiXfm, "img hi xfm" );
 //////	Bdv bdv = BdvFunctions.show( Views.stack( template, imgHiXfm ), "img hi xfm" );
 //		bdv.getBdvHandle().getSetupAssignments().getMinMaxGroups().get( 0 ).setRange( 0, 1200 );
+
 
 		System.out.println("allocating");
 		FloatImagePlus< FloatType > out = ImagePlusImgs.floats( 
@@ -180,7 +210,7 @@ public class RenderTransformed
 		}
 	}
 	
-	public static InvertibleRealTransform loadTransform( String filePath, boolean invert )
+	public static InvertibleRealTransform loadTransform( String filePath, boolean invert ) throws IOException
 	{
 		if( filePath.endsWith( "mat" ))
 		{
@@ -190,8 +220,10 @@ public class RenderTransformed
 				if( invert )
 				{
 					System.out.println("inverting");
+					System.out.println( "xfm: " + xfm );
 					return xfm.inverse().copy();
 				}
+				System.out.println( "xfm: " + xfm );
 				return xfm;
 			} catch ( IOException e )
 			{
@@ -200,31 +232,49 @@ public class RenderTransformed
 		}
 		else if( filePath.endsWith( "txt" ))
 		{
-			try
+			if( Files.readAllLines( Paths.get( filePath ) ).get( 0 ).startsWith( "#Insight Transform File" ))
 			{
-				AffineTransform xfm = AffineImglib2IO.readXfm( 3, new File( filePath ) );
-				if( invert )
+				System.out.println("Reading itk transform file");
+				try
 				{
-					System.out.println("inverting");
-					return xfm.inverse().copy();
+					AffineTransform3D xfm = ANTSLoadAffine.loadAffine( filePath );
+					if( invert )
+					{
+						System.out.println("inverting");
+						return xfm.inverse().copy();
+					}
+					return xfm;
+				} catch ( IOException e )
+				{
+					e.printStackTrace();
 				}
-				return xfm;
-				
-			} catch ( IOException e )
+			}
+			else
 			{
-				e.printStackTrace();
+				System.out.println("Reading imglib2 transform file");
+				try
+				{
+					AffineTransform xfm = AffineImglib2IO.readXfm( 3, new File( filePath ) );
+					if( invert )
+					{
+						System.out.println("inverting");
+						return xfm.inverse().copy();
+					}
+					return xfm;
+				} catch ( IOException e )
+				{
+					e.printStackTrace();
+				}
 			}
 		}
 		else
 		{
-			
-			Img< FloatType > displacement = null;
+			ImagePlus displacementIp = null;
 			if( filePath.endsWith( "nii" ))
 			{
 				try
 				{
-					displacement = ImageJFunctions.convertFloat( 
-							NiftiIo.readNifti( new File( filePath ) ) );
+					displacementIp =  NiftiIo.readNifti( new File( filePath ) );
 				} catch ( FormatException e )
 				{
 					e.printStackTrace();
@@ -235,14 +285,14 @@ public class RenderTransformed
 			}
 			else
 			{
-				displacement = ImageJFunctions.convertFloat( IJ.openImage( filePath ));
+				displacementIp = IJ.openImage( filePath );
 			}
 
-			if( displacement == null )
+			if( displacementIp == null )
 				return null;
 
-			System.out.println( "DISPLACEMENT INTERVAL: " + Util.printInterval( displacement ));
-			ANTSDeformationField dfield = new ANTSDeformationField( displacement, new double[]{ 1, 1, 1 } );
+			ANTSDeformationField dfield = new ANTSDeformationField( displacementIp );
+			System.out.println( "DISPLACEMENT INTERVAL: " + Util.printInterval( dfield.getDefInterval() ));
 
 			if( invert )
 			{
