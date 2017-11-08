504e9792a3c4f4bc23e4be28f10f96e4dfae8dc8
John Bogovic <bogovicj@janelia.hhmi.org>
2017-11-08 16:33:56 -0500

    handle resolutions correctly for position to displacement field

--- src/main/java/net/imglib2/posField/Position2DisplacementField.java
+++ src/main/java/net/imglib2/posField/Position2DisplacementField.java
@@ -1,9 +1,9 @@
-package net.
-imglib2.posField;
+package net.imglib2.posField;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -11,22 +11,19 @@ import io.nii.NiftiIo;
 import io.nii.Nifti_Writer;
 import loci.formats.FormatException;
 import net.imglib2.Cursor;
-import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealPoint;
 import net.imglib2.exception.ImgLibException;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.FloatImagePlus;
 import net.imglib2.img.imageplus.ImagePlusImgs;
+import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.ShortType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Intervals;
-import net.imglib2.util.Util;
-import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
-import process.RenderTransformed;
 
 
 /**
@@ -66,9 +63,21 @@ public class Position2DisplacementField
 		System.out.println( "reading image stack");
 		ArrayList<RandomAccessibleInterval<FloatType>> list = 
 				new ArrayList<RandomAccessibleInterval<FloatType>>( args.length - 1 );
+
+		AffineTransform3D xfm = null;
+
 		for( int i = 1; i <= 3; i++ )
 		{
-			list.add( read( args[ i ]));
+			ImgAndAffine iaa = read( args[ i ]);
+			list.add( iaa.img );
+
+			if( xfm == null )
+				xfm = iaa.affine;
+			else if( ! equals( xfm, iaa.affine ) )
+			{
+				System.err.println( "Inconsistent resolutions!" );
+				return;
+			}
 		}
 		RandomAccessibleInterval< FloatType > posField = Views.stack( list );
 
@@ -77,9 +86,19 @@ public class Position2DisplacementField
 		FloatImagePlus< FloatType > displacement = ImagePlusImgs.floats( dim );
 
 		// do the work
-		position2Displacement( posField, displacement );
+		position2Displacement( posField, displacement, xfm );
 
-		write( displacement.getImagePlus(), outArg ); 
+		ImagePlus ip = displacement.getImagePlus();
+		ip.getCalibration().pixelWidth  = xfm.get( 0, 0 );
+		ip.getCalibration().pixelHeight = xfm.get( 1, 1 );
+		ip.getCalibration().pixelDepth  = xfm.get( 2, 2 );
+
+		System.out.println( "setting calibration: " + 
+				ip.getCalibration().pixelWidth  + " " +
+				ip.getCalibration().pixelHeight  + " " + 
+				ip.getCalibration().pixelDepth );
+
+		write( ip, outArg );
 	}
 
 	public static void runSingle( String[] args ) throws ImgLibException
@@ -106,14 +125,17 @@ public class Position2DisplacementField
 			suffix="-z";
 			argIdx++;
 		}
-		
+
 		String outArg = args[ argIdx++ ];
 
 		RandomAccessibleInterval<FloatType> posField = null; 
+		AffineTransform3D xfm = null;
 		if ( args.length == argIdx + 1 )
 		{
 			System.out.println( "reading single image");
-			posField = read( args[ argIdx++ ] );
+			ImgAndAffine iaa = read( args[ argIdx++ ] );
+			posField = iaa.img;
+			xfm = iaa.affine;
 		}
 		else
 		{
@@ -122,25 +144,27 @@ public class Position2DisplacementField
 					new ArrayList<RandomAccessibleInterval<FloatType>>( args.length - 1 );
 			for( int i = argIdx; i < args.length; i++ )
 			{
-				list.add( read( args[ i ]));
+				ImgAndAffine iaa = read( args[ i ]);
+				list.add( iaa.img );
+
+				if( xfm == null )
+					xfm = iaa.affine;
+				else if( ! equals( xfm, iaa.affine ) )
+				{
+					System.err.println( "Inconsistent resolutions!" );
+					return;
+				}
 			}
 			posField = Views.stack( list );
 		}
-
-//		long[] tmp = Intervals.dimensionsAsLongArray( posField );
-//		long[] dim = new long[ tmp.length ];
-//		dim[ 0 ] = tmp[ 0 ];
-//		dim[ 1 ] = tmp[ 1 ];
-//		dim[ 2 ] = tmp[ 3 ];
-//		dim[ 3 ] = tmp[ 2 ];
 
 		long[] dim = Intervals.dimensionsAsLongArray( posField );
 		FloatImagePlus< FloatType > displacement = ImagePlusImgs.floats( dim );
 
 		if( dim_component < 0 )
-			position2Displacement( posField, displacement );
+			position2Displacement( posField, displacement, xfm );
 		else
-			position2Displacement( dim_component, posField, displacement );
+			position2Displacement( dim_component, posField, displacement, xfm );
 
 //		IntervalView< FloatType > dispPerm = Views.permute( displacement, 2, 3 );
 //		System.out.println( "posField    : " + Util.printInterval( posField ));
@@ -150,11 +174,21 @@ public class Position2DisplacementField
 
 //		IJ.save( displacement.getImagePlus(), outArg + suffix + ".tif" );
 
-		write( displacement.getImagePlus(), outArg + suffix + ".tif" ); 
+		ImagePlus ip = displacement.getImagePlus();
+		ip.getCalibration().pixelWidth  = xfm.get( 0, 0 );
+		ip.getCalibration().pixelHeight = xfm.get( 1, 1 );
+		ip.getCalibration().pixelDepth  = xfm.get( 2, 2 );
+
+		System.out.println( "setting calibration: " + 
+				ip.getCalibration().pixelWidth  + " " +
+				ip.getCalibration().pixelHeight  + " " + 
+				ip.getCalibration().pixelDepth );
+
+		write( ip, outArg + suffix + ".tif" ); 
 	}
 
 	/**
-	 * Converts a position field to a displacement field.
+	 * Converts a 3d coordinate field to a coordinate-displacement field.
 	 * 
 	 * @param posRai the position component of dimension 'dim'
 	 * @param disRai the displacement field
@@ -162,48 +196,59 @@ public class Position2DisplacementField
 	 */
 	public static <T extends RealType< T >> void position2Displacement(
 			int dim, 
-			RandomAccessibleInterval< T > posRai, RandomAccessibleInterval< T > disRai )
+			RandomAccessibleInterval< T > posRai,
+			RandomAccessibleInterval< T > disRai,
+			final AffineTransform3D xfm )
 	{
 
-		RandomAccess< T > pra = posRai.randomAccess();
-		Cursor< T > c = Views.flatIterable( disRai ).cursor();
+		final RandomAccess< T > pra = posRai.randomAccess();
+		final Cursor< T > c = Views.flatIterable( disRai ).cursor();
+		final RealPoint xfmPt = new RealPoint( pra.numDimensions() );
+
 		while ( c.hasNext() )
 		{
 			c.fwd();
 			pra.setPosition( c );
+			xfm.apply( c, xfmPt );
 
 			double dst = pra.get().getRealDouble();
 			if( dst == 0.0 )
 				continue;
 
-			double src = c.getDoublePosition( dim );
+			double src = xfmPt.getDoublePosition( dim );
 			c.get().setReal( dst - src );
 		}
 	}
 
 	/**
-	 * Converts a position field to a displacement field.
+	 * Converts a 4d position field to a displacement field.
 	 * 
 	 * @param posRai the position field
 	 * @param disRai the displacement field
+	 * @param xfm an affine transformation that
 	 */
 	public static <T extends RealType< T >> void position2Displacement(
-			RandomAccessibleInterval< T > posRai, RandomAccessibleInterval< T > disRai )
+			final RandomAccessibleInterval< T > posRai,
+			final RandomAccessibleInterval< T > disRai,
+			final AffineTransform3D xfm )
 	{
 		int nd = posRai.numDimensions();
 
-		RandomAccess< T > pra = posRai.randomAccess();
-		Cursor< T > c = Views.flatIterable( disRai ).cursor();
+		final RandomAccess< T > pra = posRai.randomAccess();
+		final Cursor< T > c = Views.flatIterable( disRai ).cursor();
+		final RealPoint xfmPt = new RealPoint( pra.numDimensions() );
+
 		while ( c.hasNext() )
 		{
 			c.fwd();
 			pra.setPosition( c );
+			xfm.apply( c, xfmPt );
 
 			double dst = pra.get().getRealDouble();
 			if( dst == 0.0 )
 				continue;
 
-			double src = c.getDoublePosition( c.getIntPosition( nd - 1 ) );
+			double src = xfmPt.getDoublePosition( c.getIntPosition( nd - 1 ) );
 			c.get().setReal( dst - src );
 		}
 	}
@@ -226,7 +271,7 @@ public class Position2DisplacementField
 			else 
 			{
 				File f = new File( filePath );
-				Nifti_Writer writer = new Nifti_Writer();
+				Nifti_Writer writer = new Nifti_Writer( true );
 				writer.save( imp, f.getParent(), f.getName() );
 			}
 
@@ -238,15 +283,15 @@ public class Position2DisplacementField
 		}
 		return false;
 	}
-	
-	public static Img<FloatType> read( String filePath )
+
+	public static ImgAndAffine read( String filePath )
 	{
+		ImagePlus ip = null;
 		if( filePath.endsWith( "nii" ))
 		{
 			try
 			{
-				return ImageJFunctions.convertFloat( 
-						NiftiIo.readNifti( new File( filePath ) ) );
+				ip = NiftiIo.readNifti( new File( filePath ) );
 			} catch ( FormatException e )
 			{
 				e.printStackTrace();
@@ -257,9 +302,45 @@ public class Position2DisplacementField
 		}
 		else
 		{
-			return ImageJFunctions.convertFloat( IJ.openImage( filePath ));
-		}	
-		return null;
-	}
-}
+			 ip = IJ.openImage( filePath );
+		}
 
+		return new ImgAndAffine( 
+				ImageJFunctions.convertFloat( ip ), 
+				ip.getCalibration().pixelWidth, 
+				ip.getCalibration().pixelHeight,
+				ip.getCalibration().pixelDepth );
+	}
+
+	public static class ImgAndAffine
+	{
+		public final Img<FloatType> img;
+		public final AffineTransform3D affine;
+
+		public ImgAndAffine( 
+				final Img<FloatType> img, 
+				final AffineTransform3D affine )
+		{
+			this.img = img;
+			this.affine = affine;
+		}
+
+		public ImgAndAffine( 
+				final Img<FloatType> img, 
+				final double... res )
+		{
+			this.img = img;
+			this.affine = new AffineTransform3D();
+			affine.set (
+					res[ 0 ], 0.0, 0.0, 0.0, 
+					0.0, res[ 1 ], 0.0, 0.0, 
+					0.0, 0.0, res[ 2 ], 0.0 );
+		}
+	}
+
+	public static boolean equals( final AffineTransform3D a, final AffineTransform3D b )
+	{
+		return Arrays.equals( a.getRowPackedCopy(), b.getRowPackedCopy() );
+	}
+
+}
--- src/main/java/net/imglib2/posField/PositionRandomAccessible.java
+++ src/main/java/net/imglib2/posField/PositionRandomAccessible.java
@@ -4,53 +4,81 @@ import net.imglib2.Interval;
 import net.imglib2.Point;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
+import net.imglib2.RealPoint;
 import net.imglib2.Sampler;
+import net.imglib2.realtransform.RealTransform;
 import net.imglib2.type.numeric.RealType;
 
-public class PositionRandomAccessible<T extends RealType< T >>
+public class PositionRandomAccessible<T extends RealType< T >, X extends RealTransform >
 		implements RandomAccessible< T >
 {
 	protected final int nd;
 	protected final T t;
 
-	public PositionRandomAccessible( int nd, T t )
+	protected X xfm;
+	protected final RealPoint pt;
+	protected final int dim;
+
+	public PositionRandomAccessible( int nd, T t, final X xfm, int dim )
 	{
 		this.nd = nd + 1;
 		this.t = t;
+		this.xfm = xfm;
+		this.dim = dim;
 		t.setZero();
+		pt = new RealPoint( nd );
 	}
 
-	public class PositionRandomAccess extends Point implements RandomAccess< T >
+	public PositionRandomAccessible( int nd, T t )
+	{
+		this( nd, t, null, -1 );
+	}
+
+	public class PositionRandomAccess< S extends RealTransform > extends Point implements RandomAccess< T >
 	{
 
-		public PositionRandomAccess( int nd )
+		protected S xfm;
+		protected final RealPoint pt;
+		protected final int dim;
+
+		public PositionRandomAccess( int nd, S xfm, int dim )
 		{
 			super( nd );
+			this.xfm = xfm;
+			this.dim = dim;
+			pt = new RealPoint( nd );
 		}
 
 		@Override
 		public T get()
 		{
-			int dim = this.getIntPosition( this.numDimensions() - 1 );
+			int d = dim;
 
-//			t.setReal( this.getDoublePosition( dim ) );
-//			return t;
+			if( d < 0 )
+				d = this.getIntPosition( this.numDimensions() - 1 );
 
 			T result = t.copy();
-			result.setReal( this.getDoublePosition( dim ) );
+
+			if( xfm == null )
+				result.setReal( this.getDoublePosition( d ) );
+			else
+			{
+				xfm.apply( this, pt );
+				result.setReal( pt.getDoublePosition( d ) );
+			}
 			return result;
 		}
 
 		@Override
 		public Sampler< T > copy()
 		{
-			return new PositionRandomAccess( this.numDimensions() );
+			return new PositionRandomAccess<S>( this.numDimensions(), xfm, dim );
 		}
 
 		@Override
 		public RandomAccess< T > copyRandomAccess()
 		{
-			return new PositionRandomAccess( this.numDimensions() );
+			return new PositionRandomAccess<S>( this.numDimensions(), xfm, dim );
 		}
 	}
 
@@ -63,12 +91,12 @@ public class PositionRandomAccessible<T extends RealType< T >>
 	@Override
 	public RandomAccess< T > randomAccess()
 	{
-		return new PositionRandomAccess( nd );
+		return new PositionRandomAccess<X>( nd, xfm, dim );
 	}
 
 	@Override
 	public RandomAccess< T > randomAccess( Interval arg0 )
 	{
-		return new PositionRandomAccess( nd );
+		return new PositionRandomAccess<X>( nd, xfm, dim );
 	}
 }
