b45c4c6bc2770de4b48ec192d3cf162aa0a1675d
John Bogovic <bogovicj@janelia.hhmi.org>
2017-11-22 09:46:07 -0500

    fix count for quickstats by label

--- python/quikstats_by_label.py
+++ python/quikstats_by_label.py
@@ -5,13 +5,7 @@ import numpy as np
 from numpy import float32, int32, uint8, dtype, genfromtxt
 
 N = len( sys.argv )
-
-
-print("woo")
-
 datPath = sys.argv[ 1 ]
-
-print(datPath)
 
 doAll = False
 if N > 2:
@@ -34,9 +28,10 @@ else:
 
     for l in labels:
         i = (dat[:,0] == l)
-        print( "%d COUNT %f" % ( l, i.shape[0] ))
-        print( "%d MEDIAN %f" % ( l, np.median( dat[i,1] )))
-        print( "%d MEAN %f" % ( l, np.mean( dat[i,1] )))
-        print( "%d STDDEV %f" % ( l, np.std( dat[i,1] )))
-        print( "%d MIN %f" % ( l, np.min( dat[i,1] )))
-        print( "%d MAX %f" % ( l, np.max( dat[i,1] )))
+        x = dat[i,1]
+        print( "%d COUNT %f" % ( l, x.shape[0] ))
+        print( "%d MEDIAN %f" % ( l, np.median( x )))
+        print( "%d MEAN %f" % ( l, np.mean( x )))
+        print( "%d STDDEV %f" % ( l, np.std( x )))
+        print( "%d MIN %f" % ( l, np.min( x )))
+        print( "%d MAX %f" % ( l, np.max( x )))
