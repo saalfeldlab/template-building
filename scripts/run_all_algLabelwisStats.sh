345973367040b057b3679b71c588838c43c6ed27
John Bogovic <bogovicj@janelia.hhmi.org>
2017-12-12 10:21:16 -0500

    helper script for alg stats

---
+++ scripts/run_all_algLabelwisStats.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source /groups/saalfeld/home/bogovicj/envbin/addMiniconda3
+source activate py36
+
+line="$1"
+merge="$2"
+N="-1"
+algList="cmtkCow cmtkCOG cmtkHideo antsRegOwl antsRegDog antsRegYang"
+template="F-antsFlip_lo"
+pyscript="/groups/saalfeld/home/bogovicj/dev/template/template-building/python/labelwise_stats_for_algPair.py"
+
+python $pyscript "$line" "$algList" "$template" "$N" "$merge"
+
+#for a1 in $algList;
+#do
+#    for a2 in $algList;
+#    do
+#        if [[ $a1 == $a2 ]];
+#        then
+#            echo "same"
+#            continue
+#        fi
+#        echo "$a1 $a2"
+#
+#        python $pyscript "$line" "$a1" "$a2" "$N"
+#    done
+#done
+
+
