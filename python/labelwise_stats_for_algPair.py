e3fbd3e1efc1012530b01b00b14e5993c73515e8
John Bogovic <bogovicj@janelia.hhmi.org>
2017-12-13 17:03:56 -0500

    add confidence intervals for mean/variance/stddev

--- python/labelwise_stats_for_algPair.py
+++ python/labelwise_stats_for_algPair.py
@@ -9,6 +9,7 @@ import itertools
 import numpy as np
 from numpy import float32, int32, uint8, dtype, genfromtxt
 
+import scipy.stats
 from scipy.stats import ttest_ind
 import pandas as pd
 
@@ -32,11 +33,16 @@ merge_labels = False
 if Nargs >= 6:
     merge_labels = str2bool(sys.argv[ 5 ])
 
+confidence=0.95
+if Nargs >= 7:
+    confidence = float(sys.argv[ 6 ])
+
 print( 'line: ', line )
 print( 'algs: ', alg_list )
 print( 'template: ', template )
 print( 'merge labels?: ', merge_labels )
 print( 'N: ', N )
+print( 'confidence: ', confidence )
 
 base_dir = '/nrs/saalfeld/john/projects/flyChemStainAtlas/all_evals'
 
@@ -85,6 +91,26 @@ for alg1,alg2 in itertools.combinations( alg_list.split(' '), 2 ):
     out_mean2_list = []
     out_tstat_list = []
     out_pval_list = [] 
+
+    out_mc1_list = []
+    out_mc1min_list = []
+    out_mc1max_list = []
+    out_vc1_list = []
+    out_vc1min_list = []
+    out_vc1max_list = []
+    out_sc1_list = []
+    out_sc1min_list = []
+    out_sc1max_list = []
+
+    out_mc2_list = []
+    out_mc2min_list = []
+    out_mc2max_list = []
+    out_vc2_list = []
+    out_vc2min_list = []
+    out_vc2max_list = []
+    out_sc2_list = []
+    out_sc2min_list = []
+    out_sc2max_list = []
  
     for label in labels:
         print( 'line: {},  label {}'.format( line, label ))
@@ -93,8 +119,11 @@ for alg1,alg2 in itertools.combinations( alg_list.split(' '), 2 ):
             df_alg1_line_label = df_alg1[ (df_alg1.LINE == line) ]
             df_alg2_line_label = df_alg2[ (df_alg2.LINE == line) ]
         else:
-            df_alg1_line_label = df_alg1[ (df_alg1.LINE == line) & (df_alg1.LABEL == label) ]
-            df_alg2_line_label = df_alg2[ (df_alg2.LINE == line) & (df_alg2.LABEL == label) ]
+            #df_alg1_line_label = df_alg1[ (df_alg1.LINE == line) & (df_alg1.LABEL == label) ]
+            #df_alg2_line_label = df_alg2[ (df_alg2.LINE == line) & (df_alg2.LABEL == label) ]
+
+            df_alg1_line_label = df_alg1[ (df_alg1.LABEL == label) ]
+            df_alg2_line_label = df_alg2[ (df_alg2.LABEL == label) ]
         
         print( df_alg1_line_label.shape )
         print( df_alg2_line_label.shape )
@@ -109,15 +138,67 @@ for alg1,alg2 in itertools.combinations( alg_list.split(' '), 2 ):
         else:
             s2 = df_alg2_line_label.sample(N)
         
+        count_1 = df_alg1_line_label.shape[0] 
+        count_2 = df_alg2_line_label.shape[0] 
+
         mn1 = s1['DISTANCE'].mean()
         mn2 = s2['DISTANCE'].mean()
-        
+       
+         
         t,p = ttest_ind( s1['DISTANCE'], s2['DISTANCE'])
+
+        if count_1 > 3:
+            mc1,vc1,sc1 = scipy.stats.bayes_mvs( s1['DISTANCE'], alpha=confidence) 
+
+            out_mc1_list += [ mc1.statistic ]
+            out_mc1min_list += [ mc1.minmax[0] ]
+            out_mc1max_list += [ mc1.minmax[1] ]
+            out_vc1_list += [ vc1.statistic ]
+            out_vc1min_list += [ vc1.minmax[0] ]
+            out_vc1max_list += [ vc1.minmax[1] ]
+            out_sc1_list += [ sc1.statistic ]
+            out_sc1min_list += [ sc1.minmax[0] ]
+            out_sc1max_list += [ sc1.minmax[1] ]
+        else:
+            print( 'skipping')
+            out_mc1_list += [ float('nan') ]
+            out_mc1min_list += [ float('nan') ]
+            out_mc1max_list += [ float('nan') ]
+            out_vc1_list += [ float('nan') ]
+            out_vc1min_list += [ float('nan') ]
+            out_vc1max_list += [ float('nan') ]
+            out_sc1_list += [ float('nan') ]
+            out_sc1min_list += [ float('nan') ]
+            out_sc1max_list += [ float('nan') ]
+
+        if count_2 > 3:
+            mc2,vc2,sc2 = scipy.stats.bayes_mvs( s2['DISTANCE'], alpha=confidence) 
+
+            out_mc2_list += [ mc2.statistic ]
+            out_mc2min_list += [ mc2.minmax[0] ]
+            out_mc2max_list += [ mc2.minmax[1] ]
+            out_vc2_list += [ vc2.statistic ]
+            out_vc2min_list += [ vc2.minmax[0] ]
+            out_vc2max_list += [ vc2.minmax[1] ]
+            out_sc2_list += [ sc2.statistic ]
+            out_sc2min_list += [ sc2.minmax[0] ]
+            out_sc2max_list += [ sc2.minmax[1] ]
+        else:
+            print( 'skipping')
+            out_mc2_list += [ float('nan') ]
+            out_mc2min_list += [ float('nan') ]
+            out_mc2max_list += [ float('nan') ]
+            out_vc2_list += [ float('nan') ]
+            out_vc2min_list += [ float('nan') ]
+            out_vc2max_list += [ float('nan') ]
+            out_sc2_list += [ float('nan') ]
+            out_sc2min_list += [ float('nan') ]
+            out_sc2max_list += [ float('nan') ]
         
         out_line_list += [ line ]
         out_label_list += [ label ]
-        out_count1_list += [ df_alg1_line_label.shape[0] ]
-        out_count2_list += [ df_alg2_line_label.shape[0] ]
+        out_count1_list += [ count_1 ]
+        out_count2_list += [ count_2 ]
         out_mean1_list += [ mn1 ]
         out_mean2_list += [ mn2 ]
         out_tstat_list += [ t ]
@@ -130,7 +211,26 @@ for alg1,alg2 in itertools.combinations( alg_list.split(' '), 2 ):
                         ('MEAN_'+alg1):out_mean1_list,
                         ('MEAN_'+alg2):out_mean2_list,
                         'TSTAT':out_tstat_list,
-                        'PVAL':out_pval_list })
+                        'PVAL':out_pval_list,
+                        ('MNSTAT_'+alg1):out_mc1_list,
+                        ('MNMIN_'+alg1):out_mc1min_list,
+                        ('MNMAX_'+alg1):out_mc1max_list,
+                        ('VRSTAT_'+alg1):out_vc1_list,
+                        ('VRMIN_'+alg1):out_vc1min_list,
+                        ('VRMAX_'+alg1):out_vc1max_list,
+                        ('SDSTAT_'+alg1):out_sc1_list,
+                        ('SDMIN_'+alg1):out_sc1min_list,
+                        ('SDMAX_'+alg1):out_sc1max_list,
+                        ('MNSTAT_'+alg2):out_mc2_list,
+                        ('MNMIN_'+alg2):out_mc2min_list,
+                        ('MNMAX_'+alg2):out_mc2max_list,
+                        ('VRSTAT_'+alg2):out_vc2_list,
+                        ('VRMIN_'+alg2):out_vc2min_list,
+                        ('VRMAX_'+alg2):out_vc2max_list,
+                        ('SDSTAT_'+alg2):out_sc2_list,
+                        ('SDMIN_'+alg2):out_sc2min_list,
+                        ('SDMAX_'+alg2):out_sc2max_list
+                        })
     
     df.to_csv( out_file )
 
