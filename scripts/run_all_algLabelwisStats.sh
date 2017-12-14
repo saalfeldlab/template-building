cc0867c5f19802bb7b3676f9cc42e5de509f6925
John Bogovic <bogovicj@janelia.hhmi.org>
2017-12-14 14:24:35 -0500

    arg for confidence interval

--- scripts/run_all_algLabelwisStats.sh
+++ scripts/run_all_algLabelwisStats.sh
@@ -5,12 +5,14 @@ source activate py36
 
 line="$1"
 merge="$2"
+conf="$3"
+
 N="-1"
 algList="cmtkCow cmtkCOG cmtkHideo antsRegOwl antsRegDog antsRegYang"
 template="F-antsFlip_lo"
 pyscript="/groups/saalfeld/home/bogovicj/dev/template/template-building/python/labelwise_stats_for_algPair.py"
 
-python $pyscript "$line" "$algList" "$template" "$N" "$merge"
+python $pyscript "$line" "$algList" "$template" "$N" "$merge" "$conf"
 
 #for a1 in $algList;
 #do
