6a9906e2139dd27b43413ee0e7c4866e4140b1d0
bogovicj <bogovicj@janelia.hhmi.org>
2018-10-29 15:55:32 -0400

    import cleanup

--- src/main/java/process/TransformPoints.java
+++ src/main/java/process/TransformPoints.java
@@ -1,62 +1,16 @@
 package process;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.janelia.utility.parse.ParseUtils;
 
-import bdv.util.Bdv;
-import bdv.util.BdvFunctions;
-import ij.IJ;
-import ij.ImagePlus;
-import io.AffineImglib2IO;
-import io.nii.NiftiIo;
 import loci.formats.FormatException;
-import net.imglib2.Cursor;
-import net.imglib2.FinalInterval;
-import net.imglib2.Interval;
-import net.imglib2.RandomAccess;
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.converter.Converter;
-import net.imglib2.converter.Converters;
-import net.imglib2.exception.ImgLibException;
-import net.imglib2.img.Img;
-import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.img.imageplus.FloatImagePlus;
-import net.imglib2.img.imageplus.ImagePlusImgs;
-import net.imglib2.img.imageplus.ShortImagePlus;
-import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
-import net.imglib2.realtransform.AffineTransform;
-import net.imglib2.realtransform.AffineTransform3D;
-import net.imglib2.realtransform.InverseRealTransform;
-import net.imglib2.realtransform.InvertibleDeformationFieldTransform;
 import net.imglib2.realtransform.InvertibleRealTransform;
 import net.imglib2.realtransform.InvertibleRealTransformSequence;
-import net.imglib2.realtransform.RealViews;
-import net.imglib2.realtransform.ants.ANTSDeformationField;
-import net.imglib2.realtransform.ants.ANTSLoadAffine;
-import net.imglib2.transform.InverseTransform;
-import net.imglib2.type.numeric.NumericType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.ShortType;
-import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
-import net.imglib2.util.Util;
-import net.imglib2.view.IntervalView;
-import net.imglib2.view.RandomAccessibleOnRealRandomAccessible;
-import net.imglib2.view.Views;
-import transforms.AffineHelper;
-import util.RenderUtil;
 
 /**
  * Renders images transformed/registered at low resolution at high resolution.
