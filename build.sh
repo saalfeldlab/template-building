034c79436049dacf4282a21db0e4b5b74c79d3b8
John Bogovic <bogovicj@janelia.hhmi.org>
2018-12-11 11:57:18 -0500

    update build script

--- build.sh
+++ build.sh
@@ -15,34 +15,8 @@ cd ..
 depdir=$(pwd)
 echo "building dependencies in: $depdir"
 
-## Grab dependencies not managed by maven yet
-git clone https://github.com/saalfeldlab/janelia-util.git
-cd janelia-util
-mvn clean compile install
-cd ..
-
-git clone https://github.com/saalfeldlab/n5-utils.git
-cd n5-utils
-mvn clean compile install
-cd ..
-
-git clone https://github.com/saalfeldlab/imglib2-transform-import.git
-cd imglib2-transform-import
-mvn -Denforcer.skip=true clean compile install
-cd ..
-
-git clone https://github.com/saalfeldlab/imglib2-transform-import.git
-cd imglib2-transform-import
-mvn -Denforcer.skip=true clean compile install
-cd ..
-
 git clone https://github.com/bogovicj/n5-imglib2.git
 cd n5-imglib2
-mvn -Denforcer.skip=true clean compile install
-cd ..
-
-git clone https://github.com/bogovicj/skeleton2swc.git
-cd skeleton2swc
 mvn -Denforcer.skip=true clean compile install
 cd ..
 
