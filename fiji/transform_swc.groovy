5f498c7343ceaf40b265a1c558ed911c46a2725a
John Bogovic <bogovicj@janelia.hhmi.org>
2019-03-01 13:32:39 -0500

    update fiji script for swc transform to new api

--- fiji/transform_swc.groovy
+++ fiji/transform_swc.groovy
@@ -1,51 +1,26 @@
 #@File(label="Swc file") swcFile
 #@File(label="Output Swc file") fileOut
-#@String(label="Transformation file path list") xfmPathList
+#@String(label="Transformation file path list") xfmPathListString
 
+import java.util.List;
 import evaluation.TransformSwc;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.realtransform.RealTransform;
+import net.imglib2.realtransform.RealTransformSequence;
 
-import net.imglib2.realtransform.InvertibleRealTransform;
-import net.imglib2.realtransform.InvertibleRealTransformSequence;
-import process.RenderTransformed;
-import sc.fiji.skeleton.SwcIO;
-import sc.fiji.skeleton.SWCPoint;
-import tracing.SNT;
+import org.janelia.saalfeldlab.swc.Swc;
+import org.janelia.saalfeldlab.swc.SwcPoint;
+import org.janelia.saalfeldlab.transform.io.TransformReader;
 
 
-InvertibleRealTransformSequence totalXfm = new InvertibleRealTransformSequence();
-boolean invert = false;
-for( p in (xfmPathList.split( "\\s+" )) )
-{
-	if( p.equals( "-i" ))
-	{
-		invert = true;
-	}
-	else
-	{
-		totalXfm.add( RenderTransformed.loadTransform( p, invert ) );
-		invert = false;
-	}
-}
+xfmPathList = xfmPathListString.split("\\s") as List
 
-res = TransformSwc.transformSWC( totalXfm, SwcIO.loadSWC( swcFile ) );
-try
-{
-	pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileOut), "UTF-8"));
-	TransformSwc.flushSWCPoints( res, pw);
-}
-catch (final IOException ioe)
-{
-	System.err.println("Saving to " + fileOut + " failed");
-}
+RealTransformSequence totalXfm = new RealTransformSequence();
+if ( xfmPathList == null || xfmPathList.size() < 1 )
+	totalXfm.add( new AffineTransform3D() );
+else
+	totalXfm = TransformReader.readTransforms( xfmPathList );
+
+res = TransformSwc.transformSWC( totalXfm, Swc.read( swcFile ));
+Swc.write( res, fileOut );
