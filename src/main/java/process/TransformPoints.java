e65597a9e176055d3bb154846605690c89f81edd
John Bogovic <bogovicj@janelia.hhmi.org>
2019-03-07 16:15:53 -0500

    update TransformPoints to new infrastructure

--- src/main/java/process/TransformPoints.java
+++ src/main/java/process/TransformPoints.java
@@ -3,113 +3,88 @@ package process;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
+import org.janelia.saalfeldlab.transform.io.TransformReader;
 import org.janelia.utility.parse.ParseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import loci.formats.FormatException;
 import net.imglib2.realtransform.InvertibleRealTransform;
 import net.imglib2.realtransform.InvertibleRealTransformSequence;
+import net.imglib2.realtransform.RealTransformSequence;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 /**
- * Renders images transformed/registered at low resolution at high resolution.
- * <p>
- * Applies up to four transformations:
- * <ul>
- * 	<li>(Optional) "Downsampling Affine" : affine from high (original) resolution to low resoultion (at which registration was performed)</li>
- *  <li>"Reg affine" : Affine part of registering transform</li>
- *  <li>"Reg Warp" : Deformable part of registering transform</li>
- *  <li>(Optional) "to canonical affine" : Final transform in final high-res space (usually brining subject to a "canonical" orientation" </li>
- * </ul>
- * <p>
- * Currently the final (implied) upsampling transform is hard-coded in : it resmples up by a factor of 4 in x and y and 2 in z.
- * After that, it upsamples z again by a factor of (~ 2.02) to resample the images to a resolution of 0.188268 um isotropic.
- * 
- * Command line parameters / usage:
- * RenderHires <high res image> <downsample affine> <reg affine> <reg warp> <output interval> <output path> <optional to canonical affine>
  * 
  * @author John Bogovic
  *
  */
-public class TransformPoints
+@Command( version = "0.0.2-SNAPSHOT" )
+public class TransformPoints implements Callable<Void>
 {
+
+	@Option( names = { "-i", "--input" }, required = true, description = "Image file to transform." )
+	private String input;
+
+	@Option( names = { "-o", "--output" }, required = false, description = "Output file. Prints to standard out if no output argument given." )
+	private String output;
+
+	@Option( names = { "-t", "--transform" }, required = false, description = "Transformation file." )
+	private List< String > transformFiles = new ArrayList<>();
+
+	@Option( names = { "-d", "--delimeter" }, required = false, description = "Delimeter separating coordinates (default=\"${DEFAULT-VALUE}\")." )
+	private String delimeter = ",";
+
+	@Option( names = { "--header" }, required = false, description = "Number of header lines." )
+	private int numHeaderLines = 0;
+
+//	@Option( names = { "-j", "--nThreads" }, required = false, description = "Number of rendering threads (default=1)" )
+//	private int nThreads = 1;
+
+	@Option( names = { "-v", "--version" }, required = false, versionHelp = true, description = "Prints version information and exits." )
+	private boolean version;
+	
+	private RealTransformSequence totalTransform;
+
+	final Logger logger = LoggerFactory.getLogger( TransformPoints.class );
 
 	public static void main( String[] args ) throws FormatException, Exception
 	{
+		CommandLine.call( new TransformPoints(), args );
+	}
 
-		String ptsF = args[ 0 ];
-		String fout = args[ 1 ];
+	public void setup()
+	{
+		totalTransform = TransformReader.readTransforms( transformFiles );
+	}
 
-		String delimeter = " ";
-		int numHeaderLines = 0;
-		
-		// Concatenate all the transforms
-		InvertibleRealTransformSequence totalXfm = new InvertibleRealTransformSequence();
+	public Void call()
+	{
+		setup();
 
-		int nThreads = 1;
-
-		int i = 2;
-		while( i < args.length )
+		List< String > lines;
+		try
 		{
-			boolean invert = false;
-			if( args[ i ].equals( "-i" ))
-			{
-				invert = true;
-				i++;
-			}
-
-			if( args[ i ].equals( "-q" ))
-			{
-				i++;
-				nThreads = Integer.parseInt( args[ i ] );
-				i++;
-				System.out.println( "argument specifies " + nThreads + " threads" );
-				continue;
-			}
-
-			if( args[ i ].equals( "-d" ))
-			{
-				i++;
-				delimeter = args[ i ];
-				i++;
-				System.out.println( "argument specifies delimeter " + delimeter );
-				continue;
-			}
-			
-			if( args[ i ].equals( "-h" ))
-			{
-				i++;
-				numHeaderLines = Integer.parseInt( args[ i ] );
-				i++;
-				System.out.println( "argument specifies header lines " + numHeaderLines );
-				continue;
-			}
-
-			if( invert )
-				System.out.println( "loading transform from " + args[ i ] + " AND INVERTING" );
-			else
-				System.out.println( "loading transform from " + args[ i ]);
-			
-			InvertibleRealTransform xfm = RenderTransformed.loadTransform( args[ i ], invert );
-			
-			if( xfm == null )
-			{	
-				System.err.println("  failed to load transform ");
-				System.exit( 1 );
-			}
-
-			totalXfm.add( xfm );
-			i++;
+			lines = Files.readAllLines( Paths.get( input ) );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+			return null;
 		}
 
-		List< String > lines = Files.readAllLines( Paths.get( ptsF ) );
 		double[] result = new double[ 3 ];
-		
+
 		int j = 0;
 		List< String > linesout = new LinkedList< String >();
-		for( String line : lines )
+		for ( String line : lines )
 		{
 			if ( j < numHeaderLines )
 			{
@@ -118,15 +93,22 @@ public class TransformPoints
 			else
 			{
 				double[] src = ParseUtils.parseDoubleArray( line, delimeter );
-				totalXfm.apply( src, result );
-				//System.out.println( Arrays.toString( result ));
-				linesout.add( result[0] + delimeter + result[1] + delimeter +result[2] );
+				totalTransform.apply( src, result );
+				linesout.add( result[ 0 ] + delimeter + result[ 1 ] + delimeter + result[ 2 ] );
 			}
 
 			j++;
 		}
-		Files.write( Paths.get( fout ), linesout );
-	}
-	
+		try
+		{
+			Files.write( Paths.get( output ), linesout );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+
+		return null;
+	}	
 
 }
