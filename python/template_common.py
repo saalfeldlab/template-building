2359ab1bc01a6d737b37de882aa7933f70c71af7
John Bogovic <bogovicj@janelia.hhmi.org>
2019-03-27 09:38:03 -0400

    update map of standardized template names

--- python/template_common.py
+++ python/template_common.py
@@ -15,18 +15,27 @@ alg_name_map = { 'antsRegDog': 'ANTs A', 'antsRegOwl' : 'ANTs B',
         'CMTK COG' : 'CMTK A', 'CMTK Hideo' : 'CMTK B', 'CMTK Cow' : 'CMTK C' }
 
 template_name_map = { 'JFRC2013_lo':'JFRC2013', 'JFRCtemplate2010' : 'JFRC2010', 
-                      'TeforBrain_f' : 'Tefor', 'F-antsFlip_lo' : 'JRC2018', 
+                      'TeforBrain_f' : 'Tefor', 'F-antsFlip_lo' : 'JRC2018',
+                      'F-antsFlip_lof' : 'JRC2018', 
                       'F-cmtkFlip_lof' : 'CMTK groupwise', 'FCWB':'FCWB',
                       'jfrc 2013' : 'JFRC2013', 'ANTs groupwise' : 'JRC2018',
-                      'jfrc 2010' : 'JFRC2010' }
+                      'jfrc 2010' : 'JFRC2010',
+                      'jrc18_1p2':'JFRC2018 (1.2um)',
+                      'jrc18_2p4':'JFRC2018 (2.4um)',
+                      'jrc18_3p6':'JFRC2018 (3.6um)',
+                      'TeforBrain_1p2iso':'Tefor (1.2um)',
+                      'TeforBrain_2p4iso':'Tefor (2.4um)',
+                      'TeforBrain_3p6iso':'Tefor (3.6um)'}
 
-def template_name( name ):
+def template_name( name_raw ):
+    name = name_raw.lstrip(' ').rstrip(' ')
     if name in template_name_map:
         return template_name_map[ name ]
     else:
         return name
 
-def alg_name( name ):
+def alg_name( name_raw ):
+    name = name_raw.lstrip(' ').rstrip(' ')
     if name in alg_name_map:
         return alg_name_map[ name ]
     else:
@@ -48,3 +57,29 @@ def get_label_string( label_id ):
     row = label_names[label_names['Stack id'] == label_id ].reset_index()
     print( row )
     return '{} : {}'.format( row.loc[0,'JFRCtempate2010.mask130819'], row.loc[0,'Name'])
+
+
+# old entries:
+#  'JFRC2010':'firebrick'
+#  'JFRC2013':'navy'
+#  'FCWB':'darkgreen'
+#  'CMTK groupwise':'gray'
+template_color_map = { 'JFRC2010':'#E66101',
+                       'JFRC2013':'#FDB863',
+                      'JFRC2013_lo':'#FDB863',
+                       'FCWB':'#B2ABD2',
+                       'Tefor':'#5E3C99',
+                       'JRC2018':'black',
+                       'CMTK groupwise':'darkorange',
+                      'jrc18_0p6':'black',
+                      'jrc18_1p2':'black',
+                      'jrc18_2p4':'black',
+                      'jrc18_3p6':'black',
+                      'F-antsFlip_lo':'black',
+                      'F-antsFlip_lof':'black',
+                      'TeforBrain_f':'#5E3C99',
+                      'TeforBrain_1p2iso':'#5E3C99',
+                      'TeforBrain_2p4iso':'#5E3C99',
+                      'TeforBrain_3p6iso':'#5E3C99'
+                     }
+
