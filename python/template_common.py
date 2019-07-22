f4353cb9400d0dd782b059dd470fb2c2523ec702
John Bogovic <bogovicj@janelia.hhmi.org>
2019-07-22 11:03:50 -0400

    move data loading into shared analysis lib

--- python/template_common.py
+++ python/template_common.py
@@ -7,14 +7,22 @@ import itertools
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
+alg_list = ['antsRegDog', 'antsRegOwl', 'antsRegYang', 'cmtkCOG', 'cmtkCow', 'cmtkHideo', 'elastixSFA', 'elastixBigfootA' ]
+template_list = [ 'JFRC2013_lo', 'JFRCtemplate2010', 'TeforBrain_f', 'F-antsFlip_lo', 'FCWB']
+
+
 # Name Map
 alg_name_map = { 'antsRegDog': 'ANTs A', 'antsRegOwl' : 'ANTs B',
         'antsRegYang' : 'ANTs C', 'cmtkCOG' : 'CMTK A',
         'cmtkHideo' : 'CMTK B', 'cmtkCow' : 'CMTK C',
         'ANTs Dog' : 'ANTs A', 'ANTs Owl' : 'ANTs B', 'ANTs Yang' : 'ANTs C',
-        'CMTK COG' : 'CMTK A', 'CMTK Hideo' : 'CMTK B', 'CMTK Cow' : 'CMTK C' }
+        'CMTK COG' : 'CMTK A', 'CMTK Hideo' : 'CMTK B', 'CMTK Cow' : 'CMTK C',
+        'elastixSFA' : 'Elastix A', 'elastixBigfootA' : 'Elastix B' }
 
-template_name_map = { 'JFRC2013_lo':'JFRC2013', 'JFRCtemplate2010' : 'JFRC2010', 
+template_name_map = { 'JFRC2013_lo':'JFRC2013', 'JFRC2013_lof':'JFRC2013', 
+                     'JFRCtemplate2010' : 'JFRC2010', 
                       'TeforBrain_f' : 'Tefor', 'F-antsFlip_lo' : 'JRC2018',
                       'F-antsFlip_lof' : 'JRC2018', 
                       'F-cmtkFlip_lof' : 'CMTK groupwise', 'FCWB':'FCWB',
@@ -33,6 +41,8 @@ def template_name( name_raw ):
         return template_name_map[ name ]
     else:
         return name
+    
+template_list_mapped = [template_name(t) for t in template_list ]
 
 def alg_name( name_raw ):
     name = name_raw.lstrip(' ').rstrip(' ')
@@ -83,3 +93,178 @@ template_color_map = { 'JFRC2010':'#E66101',
                       'TeforBrain_3p6iso':'#5E3C99'
                      }
 
+def clean_table_hdr( df ):
+
+    for col in [ x for x in df.columns if x.startswith('Unnamed') ]:
+        df = df.drop( col, axis=1 )
+
+    rename_dict = {}
+    for a,b in df.loc[ 0 ].iteritems():
+        if( b == 'count'):
+            rename_dict[a] = b
+        elif( a.startswith('DISTANCE')):
+            rename_dict[a] = '_'.join(['DISTANCE', b])
+
+    df= df.rename(columns=rename_dict)
+    df = df.drop([0])
+    return df
+
+def clean_jac_table( df ):
+
+    for col in [ x for x in df.columns if x.startswith('Unnamed') ]:
+        df = df.drop( col, axis=1 )
+
+    rename_dict = {}
+    for a,b in df.loc[ 0 ].iteritems():
+        rename_dict[a] = '_'.join(['JAC', b])
+
+    df= df.rename(columns=rename_dict)
+    df = df.drop([0])
+    return df
+
+def genStdDevs( df ):
+    df['DISTANCE_std'] = df.apply( lambda x: math.sqrt(float(x['DISTANCE_var'])), axis=1)
+    df['DISTANCE_gam_std'] = df.apply( lambda x: math.sqrt(float(x['DISTANCE_gam_var'])), axis=1)
+    df['DISTANCE_ray_std'] = df.apply( lambda x: math.sqrt(float(x['DISTANCE_ray_var'])), axis=1)
+    df.reset_index( drop=True )
+
+def readStatTables( stat_dir='/nrs/saalfeld/john/projects/flyChemStainAtlas/all_evals/stats_2019Mar'):
+    dist_df = None
+    for f in glob.glob( ''.join([stat_dir,'/*dist*.csv']) ):
+        tmp_df = pd.read_csv( f )
+        tmp_df_clean = clean_table_hdr( tmp_df )
+        if dist_df is None:
+            dist_df = tmp_df_clean
+        else:
+            dist_df = dist_df.append( tmp_df_clean )
+
+    genStdDevs( dist_df )
+
+
+    jac_df = None
+    for f in glob.glob( ''.join([stat_dir,'/*jacStats.csv']) ):
+    #     print( f )
+        tmp_df = pd.read_csv( f )
+        if jac_df is None:
+            jac_df = tmp_df
+        else:
+            jac_df = jac_df.append( tmp_df )
+
+    jac_df.rename( columns={'count':'JAC_count','mean':'JAC_mean','variance':'JAC_var','stddev':'JAC_std'}, inplace=True)
+
+    hess_df = None
+    for f in glob.glob( ''.join([stat_dir,'/*hessStats.csv']) ):
+    #     print( f )
+        tmp_df = pd.read_csv( f )
+        if hess_df is None:
+            hess_df = tmp_df
+        else:
+            hess_df = hess_df.append( tmp_df )
+
+    hess_df.rename( columns={'count':'HES_count','mean':'HES_mean','variance':'HES_var','stddev':'HES_std'}, inplace=True)
+
+
+    timeMem_df = None
+    for f in glob.glob( ''.join([stat_dir,'/*timeStats.csv']) ):
+    #     print( f )
+        tmp_df = pd.read_csv( f, names=['TEMPLATE','ALG','numThreads','runtime','avgmem', 'maxmem'] )
+        if timeMem_df is None:
+            timeMem_df = tmp_df
+        else:
+            timeMem_df = timeMem_df.append( clean_table_hdr( tmp_df ))
+    
+    return dist_df, jac_df, hess_df, timeMem_df
+
+def groupTables( dist_df, jac_df, hess_df, timeMem_df ):
+    dist_df['ALG'].unique()
+    [alg_name(x) for x in dist_df['ALG'].unique() ]
+
+    # filter templates
+    tmask = dist_df.apply( lambda x: (x['TEMPLATE'] in template_list ), axis=1)
+    df = dist_df.loc[tmask]
+
+    # Filter out appropriate rows and columns
+    dist_table = df.loc[ (df.LABEL == -1) & (df.ALG != 'ALL')][['ALG','TEMPLATE','DISTANCE_mean','DISTANCE_std']]
+
+    dist_table['TEMPLATE'] = dist_table.apply(lambda x: template_name(x['TEMPLATE']), axis=1)
+    dist_table['ALG'] = dist_table.apply(lambda x: alg_name(x['ALG']), axis=1)
+    dist_table['TA'] = dist_table.apply(lambda x: ''.join([x['TEMPLATE'],':',x['ALG']]), axis=1)
+
+
+    # Tabulate times
+    timeMem_df['CPUTIME'] = timeMem_df.apply(lambda x: float(x['runtime'])*float(x['numThreads']), axis=1) 
+    timeMem_df['CPUTIME_hr'] = timeMem_df.apply(lambda x: float( x['CPUTIME']) / 3600., axis=1) 
+    # timeMem_df['CPUTIME'] = timeMem_df.apply(lambda x: float(x['runtime']), axis=1) 
+
+    times_stats = timeMem_df.groupby(['ALG','TEMPLATE']).agg({'CPUTIME_hr' : ['mean','median','var']})
+    times_stats_flat = pd.DataFrame(times_stats.to_records())
+
+    newcols = [ re.sub('[\'(),]','', c ).replace( ' ', '_') for c in times_stats_flat.columns ]
+    times_stats_flat.columns = newcols
+    times_stats_flat['ALG'] = times_stats_flat.apply(lambda x: alg_name(x['ALG']), axis=1)
+    times_stats_flat['CPUTIME_hr_sd'] = times_stats_flat.apply(lambda x: float(math.sqrt(x['CPUTIME_hr_var'])), axis=1)
+
+
+    times_stats_flat['TEMPLATE'] = times_stats_flat.apply(lambda x: template_name(x['TEMPLATE']), axis=1)
+    times_stats_flat['TA'] = times_stats_flat.apply(lambda x: ''.join([x['TEMPLATE'],':',x['ALG']]), axis=1)
+
+    time_table = times_stats_flat
+
+    
+    # Tabulate jacobians and hessians
+    jac_table = jac_df.loc[ jac_df.label == -1 ]
+
+    jac_table['TEMPLATE'] = jac_table.apply(lambda x: template_name(x['TEMPLATE']), axis=1)
+    jac_table['ALG'] = jac_table.apply(lambda x: alg_name(x['ALG']), axis=1)
+    jac_table['TA'] = jac_table.apply(lambda x: ''.join([x['TEMPLATE'],':',x['ALG']]), axis=1)
+
+
+    hess_table = hess_df.loc[ hess_df.label == -1 ]
+
+    hess_table['TEMPLATE'] = hess_table.apply(lambda x: template_name(x['TEMPLATE']), axis=1)
+    hess_table['ALG'] = hess_table.apply(lambda x: alg_name(x['ALG']), axis=1)
+    hess_table['TA'] = hess_table.apply(lambda x: ''.join([x['TEMPLATE'],':',x['ALG']]), axis=1)
+
+    # combine the tables
+    tmp_tbl = dist_table.set_index('TA').join( time_table.set_index('TA'), lsuffix='_dist')
+    # tmp_tbl
+    total_table = tmp_tbl.join( jac_table.set_index('TA'), lsuffix='_j' )
+    total_table = total_table.join( hess_table.set_index('TA'), lsuffix='_j' )
+    # total_table
+
+
+    # total_table.query( 'label == -1')
+    # total_table.query( 'TEMPLATE == "JFRC2013" & ALG == "elastixSFA"' )
+    # total_table.query( 'ALG == "elastixSFA" & label == -1' )
+
+
+    grouped_label_table = total_table.reset_index()[['ALG','TEMPLATE','CPUTIME_hr_mean','CPUTIME_hr_sd','DISTANCE_mean','DISTANCE_std','JAC_std','HES_mean']]
+    grouped_label_table['DISTANCE_mean'] = grouped_label_table.apply(lambda x: float(x['DISTANCE_mean']), axis=1)
+
+    return grouped_label_table
+    
+
+def make_scatter_plot( table, x_label, y_label ):
+    ax = plt.gca()
+    for i,row in table.iterrows():
+
+        template = alg_name(row['TEMPLATE'].lstrip(' ').rstrip(' '))
+        alg = alg_name(row['ALG'].lstrip(' ').rstrip(' '))
+        s = "  " + alg
+
+        c = template_color_map[row['TEMPLATE']]
+        ax.annotate( s, (row[x_label], row[y_label] ), color=c, size=13 )
+
+        # The below lines only add labels for one algorithm (per template)
+        # so that duplicates don't appear in the legend
+        # Use ANTs A becuse it shows up below
+        if( alg == 'ANTs A' ):
+            p = plt.scatter( row[x_label], row[y_label], color=c, label=template )
+        else:
+            p = plt.scatter( row[x_label], row[y_label], color=c )
+
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.legend()
+
+    return ax
