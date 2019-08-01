d310c52cee04c4dfce90f1d0b74d3da8a892ed45
John Bogovic <bogovicj@janelia.hhmi.org>
2019-08-01 09:03:14 -0400

    point samples to picocli, add delimeter option

--- src/main/java/process/PointSamples.java
+++ src/main/java/process/PointSamples.java
@@ -1,26 +1,63 @@
 package process;
 
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
 import org.janelia.utility.parse.ParseUtils;
 
 import net.imglib2.FinalInterval;
 import net.imglib2.FinalRealInterval;
 import net.imglib2.RealPoint;
 import net.imglib2.iterator.IntervalIterator;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class PointSamples
+@Command( version = "0.1.1-SNAPSHOT" )
+public class PointSamples implements Callable<Void>
 {
+	@Option( names = { "-l", "--min" }, required = false, split = ",", 
+			description = "Lowest, or minimum coordinate. Default (0,..0)" )
+	private double[] min;
+
+	@Option( names = { "-h", "--max" }, required = true, split = ",", 
+			description = "Highest, or maximum coordinate. Default (0,..0)" )
+	private double[] max;
+
+	@Option( names = { "-s", "--spacing" }, required = false, split = ",", 
+			description = "Spacing. Default: (1,..1)" )
+	private double[] spacing;
+
+	@Option( names = { "-d", "--delimeter" }, required = false, description = "Delimeter" )
+	private String delimiter = ",";
 
 	public static void main( String[] args )
 	{
+		CommandLine.call( new PointSamples(), args );
+	}
 
-		String[] minmax = args[ 0 ].split( ":" );
-		double[] min = ParseUtils.parseDoubleArray( minmax[ 0 ] );
-		double[] max = ParseUtils.parseDoubleArray( minmax[ 1 ] );
+	/**
+	 * Generates point samples.
+	 * 
+	 * java process.PointSamples
+	 */
+	public Void call()
+	{
+		int nd = max.length;
+		
+		if( min == null )
+		{
+			min = new double[ nd ];
+		}
+		
+		if( spacing == null )
+		{
+			spacing = new double[ nd ];
+			Arrays.fill( spacing, 1.0 );
+		}
+
 		FinalRealInterval realInterval = new FinalRealInterval( min, max );
 
-		double[] spacing = ParseUtils.parseDoubleArray( args[ 1 ] );
-
-		int nd = realInterval.numDimensions();
 
 		long[] sampledim = new long[ nd ];
 		for ( int i = 0; i < nd; i++ )
@@ -44,12 +81,14 @@ public class PointSamples
 
 				str.append( Double.toString( p.getDoublePosition( i ) ) );
 				if ( i < nd - 1 )
-					str.append( "," );
+					str.append( delimiter );
 
 			}
 			System.out.println( str.toString() );
 
 		}
+
+		return null;
 	}
 
 }
